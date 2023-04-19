dd58677cf40e68dca2ca542a5d07d157673c9f90
liyiwen <johnnyeven@gmail.com>
2023-04-19 16:06:23 +0800

    db

--- api/schedule_task.go
+++ api/schedule_task.go
@@ -3,7 +3,5 @@ package api
 import "net/http"
 
 func ScheduleTask(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "success"}`))
+	responseEmpty(w)
 }
